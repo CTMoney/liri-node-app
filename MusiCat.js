d71114ea46654c31e5a26f7e324d42540f4e449f
Cat <money1062@live.com>
2019-02-04 13:58:59 -0500

    nearly finished BIT call

--- MusiCat.js
+++ MusiCat.js
@@ -1,115 +1,72 @@
 var Spotify = require("node-spotify-api");
 var dotenv = require("dotenv").config();
-var options_primary = require("./options.js");
+var options = require("./options.js");
 var inquirer = require("inquirer");
 var keys = require("./keys.js");
 var moment = require("moment");
+var figlet = require("figlet");
 var axios = require("axios");
 var clear = require("clear");
-var figlet = require("figlet");
+
 var spotify = new Spotify(keys.spotify);
 
-inquirer.prompt(options_primary).then(function(response) {
-  if (response.program_select == "Concert This!") {
-    inquirer.prompt(concertQuery).then(function(concertResponse) {
-      axios
-        .get(
-          `https://rest.bandsintown.com/artists/${
-            response.artist
-          }/events?app_id=codingbootcamp`
-        )
-        .then(function(error, data) {});
-    });
-  } else if (response.program_select == "Spotify This!") {
-    inquirer.prompt(spotifyQuery).then(function(spotifyResponse) {
-      axios.get().then(function(error, data) {});
-    });
-  } else if (response.program_select == "Movie This!") {
-    inquirer.prompt(spotifyQuery).then(function(spotifyResponse) {
-      axios.get().then(function(error, data) {});
-    });
-  } else if (response.program_select == "Do what It Says") {
-    clear();
 
-    var randomChoice;
-    var counter = 0;
 
-    let mathVisual = setInterval(() => {
-      randomChoice = Math.floor(Math.random() * 3);
-      let programChoiceArray = [
-        "Concert This!",
-        "Spotify This!",
-        "Movie This!"
-      ];
 
-      clear();
 
-      figlet(
-        `                    ${programChoiceArray[randomChoice]}`,
-        function(err, data) {
-          if (err) {
-            throw err;
-          }
 
-          console.log(`\n\n\n\n\n\n\n\n\n\n${data}`);
-        }
-      );
-      setTimeout(() => {
-        clear();
-      }, 750);
+inquirer
+  .prompt(options.primary)
+  .then(function (response) {
 
-      counter++;
-      if (counter == 3) {
-        clearInterval(mathVisual);
-        let flashingCounter = 0;
-        let flashingInterval = setInterval(() => {
-          if (flashingCounter % 2 == 0) {
-            figlet(
-              `                    ${programChoiceArray[randomChoice]}!!`,
-              function(err, data) {
-                if (err) {
-                  throw err;
-                }
-                console.log(`\n\n\n\n\n\n\n\n\n\n${data}`);
-              }
-            );
+    if (response.program_select == "Concert This!") {
 
-            flashingCounter++;
-          } else {
-            clear();
-            flashingCounter++;
-          }
+      inquirer
+        .prompt(concertQuery)
+        .then(function (concertResponse) {
+          axios
+            .get(`https://rest.bandsintown.com/artists/${concertResponse.concert_artist}/events?app_id=codingbootcamp`)
+            .then(function (bitResponse, err) {
+                if(err){throw err}
+                let bit = bitResponse.data[0];
+                let { venue, datetime, url} = bit;
+                let {name, city, country} = venue;
+                let time = moment(datetime, "YYYY-MM-DDTHH:mm:ss").format("LLLL")
 
-          if (flashingCounter == 20) {
-            figlet("Selection Made!"),
-              function(err, data) {
-                if (err) {
-                  throw err;
-                }
-                console.log(data);
-              };
+                console.log(`${name}\n${city}, ${country}`);
+                console.log(time);
+                console.log(url);
+                
+                
+            });
+        });
+    }
 
-            clearInterval(flashingInterval);
+    else if (response.program_select == "Spotify This!") {
 
-            setTimeout(() => {
-              clear();
-              switch (randomChoice) {
-                case randomChoice == 1:
-                  concertThis();
-                  break;
-                case randomChoice == 2:
-                  spotifyThis();
-                  break;
-                case randomChoice == 3:
-                  movieThis();
-                  break;
-                default:
-                  break;
-              }
-            }, 2500);
-          }
-        }, 200);
-      }
-    }, 1000);
-  }
-});
+      inquirer
+        .prompt(options.spotifyQuery)
+        .then(function (spotifyResponse) {
+
+          axios
+            .get()
+            .then(function (err, data) { });
+        });
+    }
+
+    else if (response.program_select == "Movie This!") {
+
+      inquirer
+        .prompt(options.movieQuery)
+        .then(function (spotifyResponse) {
+
+          axios
+            .get()
+            .then(function (err, data) { });
+        });
+    }
+
+    else if (response.program_select == "Do what It Says") {
+
+    }
+  });
--- options.js
+++ options.js
@@ -1,4 +1,4 @@
-options_primary = [
+primary = [
   {
     type: "list",
     message: "Select a program!",
@@ -12,7 +12,36 @@ options_primary = [
   }
 ];
 
-module.exports = options_primary;
+concertQuery = [
+  {
+    type: "input",
+    message: "Please enter an artist / band name",
+    name: "concert_artist"
+  }
+]
+
+spotifyQuery = [
+  {
+    type: "input",
+    message: "Please enter a song name",
+    name: "spotify_song"
+  }
+]
+
+movieQuery = [
+  {
+    type: "input",
+    message: "Please enter a movie name",
+    name: "movie_title"
+  }
+]
+
+module.exports = {
+  primary,
+  concertQuery,
+  spotifyQuery,
+  movieQuery
+};
 
 // * `concert-this`
 
