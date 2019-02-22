f60a4cae68a4cafd292477b81187ce35378993b6
CTMoney <money1062@live.com>
2019-02-21 21:10:49 -0500

    .

--- liri.js
+++ liri.js
@@ -2,6 +2,17 @@ var dotenv = require("dotenv").config();
 var options = require("./assets/javascript/options.js");
 var inquirer = require("inquirer");
 var clear = require("clear");
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.listen(PORT, () =>{
+  console.log("App Listening on PORT: " + PORT);
+  
+})
 
 var call_functions = require("./assets/javascript/call-functions");
 var { bitCall, spotifyCall, omdbCall, dwisCall } = call_functions;
@@ -30,4 +41,7 @@ inquirer.prompt(options.primary).then(function (response) {
 
 });
 
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
